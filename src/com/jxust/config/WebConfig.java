bf989780ca7592ab4b05715a047551c7231e74fe
jxust-CGS <1055038041@qq.com>
2014-10-17 22:30:28 +0800

    初次提交部分jfinal框架

---
+++ src/com/jxust/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.jxust.config;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+import com.jxust.controller.HelloController;
+
+public class WebConfig extends JFinalConfig
+{
+	public void configConstant(Constants me) 
+	{
+		me.setDevMode(true);
+	}
+
+	public void configRoute(Routes me) 
+	{
+		me.add("/hello", HelloController.class);
+	}
+	
+	public void configPlugin(Plugins me) {}
+	public void configInterceptor(Interceptors me) {}
+	public void configHandler(Handlers me) {}
+}
